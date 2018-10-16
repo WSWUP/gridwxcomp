34caed51b647e12b6ee44f3c7a1df09e79bfff4c
Chris Pearson <cpearson@dri.edu>
2018-10-16 10:55:02 -0700

    Timestamp Formatting
    Timestamp Formatting for consistent input/output.
    Duplicate removal.
    Added unique year filter, to optimize downloading redundant data.

--- grid_pull_append.py
+++ grid_pull_append.py
@@ -1,19 +1,19 @@
-import pandas as pd
 import datetime as dt
 import ee
 import numpy as np
+import os
+import pandas as pd
+import refet
+import sys
 from time import sleep
 import timeit
-import os
-import sys
-import refet
 
 ee.Initialize()
 
-os.chdir('D:\etr-biascorrect')
+os.chdir('C:\etr-biascorrect')
 
 # Input .csv containing GRIDMET_ID, LAT, LON
-input_df = pd.read_csv('D:\etr-biascorrect\gridmet_testlist.txt')
+input_df = pd.read_csv('C:\etr-biascorrect\gridmet_testlist.txt')
 
 # List of ee GRIDMET varibles to retrieve
 # https://explorer.earthengine.google.com/#detail/IDAHO_EPSCOR%2FGRIDMET
@@ -26,7 +26,7 @@ met_names= ['Tmax', 'Tmin', 'Srad_wm2', 'Ws_10m', 'q_kgkg', 'RH_min', 'RH_max',
 
 # Specify column order for output .csv Variables:
 output_order = ['Date', 'Year', 'Month', 'Day', 'Elev_m', 'pair_kPa', 'Ws2m_ms',
-                'Tmin_C', 'Tmax_C', 'Srad_wm2', 'q_kgkg', 'ea_kPa', 'RH_min',
+                'Tmin_C', 'Tmax_C', 'Tavg_C', 'Srad_wm2', 'q_kgkg', 'ea_kPa', 'RH_min',
                 'RH_max', 'RH_avg', 'Prcp_mm', 'ETr_mm', 'ETo_mm']
 
 # Exponential getinfo call from ee-tools/utils.py
@@ -44,6 +44,7 @@ def ee_getinfo(ee_obj, n=30):
             break
     return output
 
+
 # Loop through dataframe row by row and grab desired met data from
 # GRID collections based on Lat/Lon and Start/End dates
 for index, row in input_df.iterrows():
@@ -53,7 +54,6 @@ for index, row in input_df.iterrows():
     export_df = None
     
     GRIDMET_ID_str=str(row.GRIDMET_ID)
-    # print(['Cell Loop Counter',index+1])
     print('Processing GRIDMET ID: {}'.format(GRIDMET_ID_str))
 
     # determine end date of data collection
@@ -61,10 +61,10 @@ for index, row in input_df.iterrows():
     end_date = dt.date(current_date.year, current_date.month,
                        current_date.day-1)
     # Create List of all dates
-    full_date_list = pd.date_range(dt.datetime.strptime('2016-01-01',
+    full_date_list = pd.date_range(dt.datetime.strptime('2014-01-01',
                                                         '%Y-%m-%d'), end_date)
     output_name = 'gridmet_historical_' + GRIDMET_ID_str + '.csv'
-    output_file = os.path.join('D:\etr-biascorrect', output_name)
+    output_file = os.path.join('C:\etr-biascorrect', output_name)
     
     if os.path.isfile(output_file):
         print('{} Exists. Checking for missing data.'.format(output_name))
@@ -72,8 +72,8 @@ for index, row in input_df.iterrows():
         missing_dates = list(set(full_date_list) - set(pd.to_datetime(
             original_df['Date'])))
         original_df.Date = pd.to_datetime(original_df.Date.astype(str),
-                                        format='%Y%m%d')
-        original_df.Date.apply(lambda x: x.strftime('%Y-%m-%d'))
+                                          format='%Y-%m-%d')
+        original_df = original_df.Date.apply(lambda x: x.strftime('%Y-%m-%d'))
     else:
         print('{} does not exists. Creating file.'.format(output_name))
         missing_dates = list(set(full_date_list))
@@ -84,6 +84,11 @@ for index, row in input_df.iterrows():
     # Min and Max of Missing Dates (Start: Inclusive; End: Exclusive)
     start_date = min(missing_dates)
     end_date = max(missing_dates)
+
+    missing_years = []
+    for date in missing_dates:
+        missing_years = missing_years + [date.year]
+    missing_years = sorted(list(set(missing_years)))
 
     # Create ee point from lat and lon
     point = ee.Geometry.Point(row.LON, row.LAT)
@@ -100,7 +105,8 @@ for index, row in input_df.iterrows():
     # Process dataframe units and output after                        
     start_date_yr = start_date.year
     end_date_yr = end_date.year                             
-    for iter_year in range(start_date_yr, end_date_yr+1, 1):
+    # for iter_year in range(start_date_yr, end_date_yr+1, 1):
+    for iter_year in missing_years:
         print(iter_year)                               
     # Filter Collection by Start/End Date and Lat/Lon Point
     # Only include 'permanent' data
@@ -160,16 +166,13 @@ for index, row in input_df.iterrows():
     # export_df['DOY'] = export_df['Date'].dt.dayofyear
     # Format Date for export
     export_df['Date'] = export_df.Date.apply(lambda x: x.strftime('%Y-%m-%d'))
-    print(export_df.Date[1])
-    print(type(export_df.Date[1]))
     # Remove all negative Prcp values (GRIDMET Bug)
     export_df.Prcp_mm = export_df.Prcp_mm.clip(lower=0)
 
     # Convert 10m windspeed to 2m (ASCE Eqn. 33)
     zw = 10
-    #export_df['Ws_2m'] = export_df.Ws_10m*((4.87/np.log(67.8*zw-5.42)))
     export_df['Ws2m_ms'] = refet.calcs._wind_height_adjust(export_df.Ws_10m, zw)
-    #elevation from gridMET elevation layer
+    # elevation from gridMET elevation layer
     export_df['Elev_m'] = elev
 
     # air pressure from gridmet elevation using refet module
@@ -183,7 +186,8 @@ for index, row in input_df.iterrows():
     # Unit Conversions
     export_df.Tmax = export_df.Tmax-273.15  #K to C
     export_df.Tmin = export_df.Tmin-273.15  #K to C
-    export_df.rename(columns = {'Tmax': 'Tmax_C','Tmin':'Tmin_C'}, inplace=True)
+    export_df.rename(columns={'Tmax': 'Tmax_C', 'Tmin': 'Tmin_C'}, inplace=True)
+    export_df['Tavg_C'] = (export_df.Tmax_C + export_df.Tmin_C)/2
 
     # Relative Humidity from gridMET min and max
     export_df['RH_avg'] = (export_df.RH_max + export_df.RH_min)/2
@@ -194,7 +198,7 @@ for index, row in input_df.iterrows():
     export_df = export_df.sort_values(by=['Year', 'Month', 'Day'])
     
     # Write csv files to working directory
-    export_df.to_csv(output_name, columns = output_order, index=False)
+    export_df.to_csv(output_name, columns=output_order, index=False)
     elapsed = timeit.default_timer() - start_time
     print(elapsed)   
 
