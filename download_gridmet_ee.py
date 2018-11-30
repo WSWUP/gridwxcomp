c0f9b5b50c67e066603c7dc147c526cb6e13a2bd
Chris Pearson <cpearson@dri.edu>
2018-11-30 08:39:05 -0800

    PEP8 Edits
    General Clean-up

--- download_gridmet_ee.py
+++ download_gridmet_ee.py
@@ -1,14 +1,14 @@
 import argparse
 import datetime as dt
-import ee
 import logging
-import numpy as np
 import os
+import sys
+import timeit
+from time import sleep
+
+import ee
 import pandas as pd
 import refet
-import sys
-from time import sleep
-import timeit
 
 ee.Initialize()
 
@@ -37,17 +37,17 @@ def main(input_csv, out_folder, year_filter=''):
                  'pr', 'etr', 'eto']
 
     # Rename GRIDMET variables during ee export
-    met_names= ['tmax', 'tmin', 'srad_wm2', 'u10_ms', 'q_kgkg', 'rh_min', 'rh_max',
-                'prcp_mm', 'etr_mm', 'eto_mm']
+    met_names = ['tmax', 'tmin', 'srad_wm2', 'u10_ms', 'q_kgkg', 'rh_min',
+                 'rh_max', 'prcp_mm', 'etr_mm', 'eto_mm']
 
     # Specify column order for output .csv Variables:
-    output_order = ['date', 'year', 'month', 'day', 'centroid_lat', 'centroid_lon',
-                    'elev_m', 'u2_ms', 'tmin_c', 'tmax_c', 'srad_wm2', 'ea_kpa',
-                    'prcp_mm', 'etr_mm', 'eto_mm']
+    output_order = ['date', 'year', 'month', 'day', 'centroid_lat',
+                    'centroid_lon', 'elev_m', 'u2_ms', 'tmin_c', 'tmax_c',
+                    'srad_wm2', 'ea_kpa', 'prcp_mm', 'etr_mm', 'eto_mm']
 
-    #Year Filter
+    # Year Filter
     if year_filter:
-            year_list= sorted(list(parse_int_set(year_filter)))
+            year_list = sorted(list(parse_int_set(year_filter)))
             logging.info('\nDownloading Years: {0}-{1}'.format(min(year_list),
                                                                max(year_list)))
             date_list = pd.date_range(
@@ -65,8 +65,9 @@ def main(input_csv, out_folder, year_filter=''):
         date_list = pd.date_range(dt.datetime.strptime('1979-01-01',
                                                        '%Y-%m-%d'), end_date)
     # Exponential getinfo call from ee-tools/utils.py
+
     def ee_getinfo(ee_obj, n=30):
-        """Make an exponential backoff getInfo call on the Earth Engine object"""
+        """Make an exponential backoff getInfo call on the EarthEngine object"""
         output = None
         for i in range(1, n):
             try:
@@ -79,7 +80,6 @@ def main(input_csv, out_folder, year_filter=''):
                 break
         return output
 
-
     # Loop through dataframe row by row and grab desired met data from
     # GRID collections based on Lat/Lon and Start/End dates
     for index, row in input_df.iterrows():
@@ -88,22 +88,25 @@ def main(input_csv, out_folder, year_filter=''):
         original_df = None
         export_df = None
 
-        GRIDMET_ID_str=str(row.GRIDMET_ID)
+        GRIDMET_ID_str = str(row.GRIDMET_ID)
         logging.info('Processing GRIDMET ID: {}'.format(GRIDMET_ID_str))
 
         output_name = 'gridmet_historical_' + GRIDMET_ID_str + '.csv'
         output_file = os.path.join(out_folder, output_name)
 
         if os.path.isfile(output_file):
-            logging.info('{} exists. Checking for missing data.'.format(output_name))
+            logging.info('{} exists. Checking for missing data.'.format(
+                output_name))
             original_df = pd.read_csv(output_file, parse_dates=True)
             missing_dates = list(set(date_list) - set(pd.to_datetime(
                 original_df['date'])))
             original_df.date = pd.to_datetime(original_df.date.astype(str),
                                               format='%Y-%m-%d')
-            original_df['date'] = original_df.date.apply(lambda x: x.strftime('%Y-%m-%d'))
+            original_df['date'] = original_df.date.apply(lambda x: x.strftime(
+                '%Y-%m-%d'))
         else:
-            logging.info('{} does not exists. Creating file.'.format(output_name))
+            logging.info('{} does not exists. Creating file.'.format(
+                output_name))
             missing_dates = list(set(date_list))
         if not missing_dates:
             logging.info('No missing data found. Skipping')
@@ -119,13 +122,14 @@ def main(input_csv, out_folder, year_filter=''):
         missing_years = sorted(list(set(missing_years)))
 
         # Add check to verify lat/lon fall within the gridmet extent
-        # -124.78749996666667 25.04583333333334 -67.03749996666667 49.42083333333334
+        # -124.78749996666667 25.04583333333334
+        # -67.03749996666667 49.42083333333334
         # Create ee point from lat and lon
         point = ee.Geometry.Point(row.LON, row.LAT)
 
         # gridmet elevation image:
         # ee.Image('projects/climate-engine/gridmet/elevation')
-        elev= ee.Image('projects/climate-engine/gridmet/elevation') \
+        elev = ee.Image('projects/climate-engine/gridmet/elevation') \
             .reduceRegion(reducer=ee.Reducer.mean(), geometry=point,
                           scale=4000)
         elev = ee_getinfo(elev)['b1']
@@ -135,10 +139,12 @@ def main(input_csv, out_folder, year_filter=''):
         gridmet_lon = -124.78749996666667
         gridmet_lat = 25.04583333333334
         gridmet_cs = 0.041666666666666664
-        gridcell_lat = int(abs(row.LAT - gridmet_lat) / gridmet_cs) * gridmet_cs +\
-                       gridmet_lat + gridmet_cs/2
-        gridcell_lon = int(abs(row.LON - gridmet_lon) / gridmet_cs) * gridmet_cs +\
-                       gridmet_lon + gridmet_cs/2
+        gridcell_lat = int(
+            abs(row.LAT - gridmet_lat) / gridmet_cs) * gridmet_cs +\
+            gridmet_lat + gridmet_cs/2
+        gridcell_lon = int(
+            abs(row.LON - gridmet_lon) / gridmet_cs) * gridmet_cs +\
+            gridmet_lon + gridmet_cs/2
 
         # Loop through ee pull by year (max 5000 records for getInfo())
         # Append each new year on end of dataframe
@@ -156,8 +162,8 @@ def main(input_csv, out_folder, year_filter=''):
                 .select(met_bands, met_names)
 
             # Check if collection is empty
-            image_count = ee.Number(gridmet_coll.limit(1) \
-                                    .reduceColumns('count', ['system:index']) \
+            image_count = ee.Number(gridmet_coll.limit(1)
+                                    .reduceColumns('count', ['system:index'])
                                     .get('count'))
             empty = ee.Algorithms.If(image_count.eq(1), False, True)
 
@@ -173,23 +179,24 @@ def main(input_csv, out_folder, year_filter=''):
                     datestr.format("YYYYMMdd"))).rename(['date'])
                 # Add dateNum Band to Image
                 image = image.addBands([datenum])
-                # Reduce image taking mean of all pixels in geometry (4km resolution)
+                # Reduce image taking mean of all pixels in geometry (4km res)
                 input_mean = ee.Image(image) \
                     .reduceRegion(
-                            reducer=ee.Reducer.mean(), geometry=point, scale=4000)
+                            reducer=ee.Reducer.mean(), geometry=point,
+                            scale=4000)
                 return ee.Feature(None, input_mean)
             # Run get_values function over all images in gridmet collection
             data = gridmet_coll.map(get_values)
 
-            # Export dictionary to pandas dataframe using exponential getInfo fnc
+            # Export dictionary to pandas dataframe using exponential getInfo
             if iter_year == start_date_yr:
                     export_df = pd.DataFrame([
                             ftr['properties']
                             for ftr in ee_getinfo(data)['features']])
             else:
                     export_df2 = pd.DataFrame([
-                    ftr['properties']
-                    for ftr in ee_getinfo(data)['features']])
+                        ftr['properties']
+                        for ftr in ee_getinfo(data)['features']])
                     export_df = export_df.append(export_df2)
         # If export_df is None (skip to next ID)
         if export_df is None:
@@ -198,20 +205,23 @@ def main(input_csv, out_folder, year_filter=''):
         export_df = export_df.reset_index(drop=False)
 
         # Convert dateNum to datetime and create Year, Month, Day, DOY variables
-        export_df.date = pd.to_datetime(export_df.date.astype(str), format='%Y%m%d')
+        export_df.date = pd.to_datetime(export_df.date.astype(str),
+                                        format='%Y%m%d')
         export_df['year'] = export_df['date'].dt.year
         export_df['month'] = export_df['date'].dt.month
         export_df['day'] = export_df['date'].dt.day
         # export_df['DOY'] = export_df['Date'].dt.dayofyear
         # Format Date for export
-        export_df['date'] = export_df.date.apply(lambda x: x.strftime('%Y-%m-%d'))
+        export_df['date'] = export_df.date.apply(lambda x: x.strftime(
+            '%Y-%m-%d'))
 
         # Remove all negative Prcp values (GRIDMET Bug)
         export_df.prcp_mm = export_df.prcp_mm.clip(lower=0)
 
         # Convert 10m windspeed to 2m (ASCE Eqn. 33)
         zw = 10
-        export_df['u2_ms'] = refet.calcs._wind_height_adjust(export_df.u10_ms, zw)
+        export_df['u2_ms'] = refet.calcs._wind_height_adjust(
+            export_df.u10_ms, zw)
         # elevation from gridMET elevation layer
         export_df['elev_m'] = elev
         export_df['centroid_lat'] = gridcell_lat
@@ -222,34 +232,37 @@ def main(input_csv, out_folder, year_filter=''):
                                                           method='asce')
 
         # actual vapor pressure (kg/kg) using refet module
-        export_df['ea_kpa'] = refet.calcs._actual_vapor_pressure(export_df.q_kgkg,
-                                                                 export_df.pair_kpa)
+        export_df['ea_kpa'] = refet.calcs._actual_vapor_pressure(
+            export_df.q_kgkg, export_df.pair_kpa)
 
         # Unit Conversions
-        export_df.tmax = export_df.tmax-273.15  #K to C
-        export_df.tmin = export_df.tmin-273.15  #K to C
-        export_df.rename(columns={'tmax': 'tmax_c', 'tmin': 'tmin_c'}, inplace=True)
+        export_df.tmax = export_df.tmax-273.15  # K to C
+        export_df.tmin = export_df.tmin-273.15  # K to C
+        export_df.rename(columns={'tmax': 'tmax_c', 'tmin': 'tmin_c'},
+                         inplace=True)
         # export_df['Tavg_C'] = (export_df.Tmax_C + export_df.Tmin_C)/2
 
         # Relative Humidity from gridMET min and max
         # export_df['RH_avg'] = (export_df.RH_max + export_df.RH_min)/2
 
         # Add new data to original dataframe, remove duplicates
-        export_df = pd.concat([original_df, export_df], ignore_index=True, sort=True)
+        export_df = pd.concat([original_df, export_df], ignore_index=True,
+                              sort=True)
         export_df = export_df[output_order].drop_duplicates('date')
         export_df = export_df.sort_values(by=['year', 'month', 'day'])
         export_df = export_df.dropna()
-
 
         # Write csv files to working directory
         export_df.to_csv(output_file, columns=output_order, index=False)
         elapsed = timeit.default_timer() - start_time
         logging.info('\nDownload Time: {}'.format(elapsed))
 
+
 def parse_int_set(nputstr=""):
     """Return list of numbers given a string of ranges
 
-    http://thoughtsbyclayg.blogspot.com/2008/10/parsing-list-of-numbers-in-python.html
+    http://thoughtsbyclayg.blogspot.com/2008/10/
+    parsing-list-of-numbers-in-python.html
     """
     selection = set()
     invalid = set()
@@ -278,6 +291,7 @@ def parse_int_set(nputstr=""):
     # print "Invalid set: " + str(invalid)
     return selection
 
+
 def arg_parse():
     """"""
     parser = argparse.ArgumentParser(
@@ -298,6 +312,7 @@ def arg_parse():
     args = parser.parse_args()
     return args
 
+
 if __name__ == '__main__':
     args = arg_parse()
 
@@ -312,9 +327,10 @@ if __name__ == '__main__':
     main(input_csv=args.input, out_folder=args.out,
          year_filter=args.years)
 
-
-
     # Saturated vapor pressure
-    # export_df['esat_min_kPa'] = refet.calcs._sat_vapor_pressure(export_df.Tmin_C)
-    # export_df['esat_max_kPa'] = refet.calcs._sat_vapor_pressure(export_df.Tmax_C)
-    # export_df['esat_avg_kPa'] = (export_df.esat_min_kPa + export_df.esat_max_kPa)/2
+    # export_df['esat_min_kPa'] =
+    # refet.calcs._sat_vapor_pressure(export_df.Tmin_C)
+    # export_df['esat_max_kPa'] =
+    # refet.calcs._sat_vapor_pressure(export_df.Tmax_C)
+    # export_df['esat_avg_kPa'] =
+    # (export_df.esat_min_kPa + export_df.esat_max_kPa)/2
