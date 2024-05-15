b8250919ee8217c0686cb1335a545112b1a837f2
John Volk <johnvolk08@gmail.com>
2024-05-15 16:50:49 -0400

    add earth engine token reader for pytest in conf file

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,30 @@
 Add the '--runslow' command line option to pytest else skip slow markers
 """
 import pytest
+import base64
+import logging
+import os
+
+import ee
+
+
+@pytest.fixture(scope="session", autouse=True)
+def test_init():
+    logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+    logging.getLogger('googleapiclient').setLevel(logging.ERROR)
+    logging.debug('Test Setup')
+
+    # For GitHub Actions authenticate using private key environment variable
+    if 'EARTHENGINE_TOKEN' in os.environ:
+        print('Writing privatekey.json from environmental variable ...')
+        content = base64.b64decode(os.environ['EARTHENGINE_TOKEN']).decode('ascii')
+        EE_KEY_FILE = 'privatekey.json'
+        with open(EE_KEY_FILE, 'w') as f:
+            f.write(content)
+        ee.Initialize(ee.ServiceAccountCredentials('', key_file=EE_KEY_FILE))
+    else:
+        ee.Initialize()
+
 
 
 def pytest_addoption(parser):
