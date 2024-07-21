bf979240e9addf226c42d0f6b697b9359fc318f3
John Volk <john.volk@dri.edu>
2024-07-21 17:43:32 -0400

    remove bokeh sphinx extension for docs build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -62,7 +62,6 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'bokeh.sphinxext.bokeh_plot'
 ]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
